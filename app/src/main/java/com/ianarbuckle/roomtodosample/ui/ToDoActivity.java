521ed8a8625a1e442a7868100a67a2527f490daf
Ian Arbuckle <ian.arbuckle18@gmail.com>
2017-09-25 23:12:02 +0100

    - Initial commit

--- app/src/main/java/com/ianarbuckle/roomtodosample/ui/ToDoActivity.java
+++ app/src/main/java/com/ianarbuckle/roomtodosample/ui/ToDoActivity.java
@@ -2,14 +2,60 @@ package com.ianarbuckle.roomtodosample.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
+import com.ianarbuckle.roomtodosample.MyApplication;
 import com.ianarbuckle.roomtodosample.R;
+import com.ianarbuckle.roomtodosample.room.entity.Task;
+import com.ianarbuckle.roomtodosample.ui.adapter.ToDoAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class ToDoActivity extends AppCompatActivity {
+
+  @BindView(R.id.recyclerView)
+  RecyclerView recyclerView;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_todo);
+
+    ButterKnife.bind(this);
+
+    new Thread(() -> {
+      List<Task> tasks = MyApplication.get().getDatabase().taskDao().getAllTasks();
+      if(tasks.isEmpty()) {
+        fetchTasks();
+      } else {
+        populateTasks(tasks);
+      }
+    }).start();
+  }
+
+  private void fetchTasks() {
+    List<Task> tasks = new ArrayList<>();
+
+    for(int i = 0; i < 5; i ++) {
+      Task task = new Task();
+      task.setTitle("Pickle");
+      task.setDescription("Turn myself into a pickle" + i);
+      task.setPriority("Major");
+      tasks.add(task);
+    }
+
+    MyApplication.get().getDatabase().taskDao().insertAllTasks(tasks);
+
+    populateTasks(tasks);
+
+  }
+
+  private void populateTasks(List<Task> tasks) {
+    runOnUiThread(() -> recyclerView.setAdapter(new ToDoAdapter(tasks)));
   }
 }
--- app/src/main/java/com/ianarbuckle/roomtodosample/ui/adapter/ToDoAdapter.java
+++ app/src/main/java/com/ianarbuckle/roomtodosample/ui/adapter/ToDoAdapter.java
@@ -1,12 +1,18 @@
 package com.ianarbuckle.roomtodosample.ui.adapter;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.ianarbuckle.roomtodosample.R;
 import com.ianarbuckle.roomtodosample.room.entity.Task;
 
 import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by Ian Arbuckle on 25/09/2017.
@@ -23,12 +29,13 @@ public class ToDoAdapter extends RecyclerView.Adapter<ToDoAdapter.ToDoViewHolder
 
   @Override
   public ToDoViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-    return null;
+    View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_todo, parent);
+    return new ToDoViewHolder(view);
   }
 
   @Override
   public void onBindViewHolder(ToDoViewHolder holder, int position) {
-
+    holder.bind(tasks.get(position));
   }
 
   @Override
@@ -37,9 +44,24 @@ public class ToDoAdapter extends RecyclerView.Adapter<ToDoAdapter.ToDoViewHolder
   }
 
   static class ToDoViewHolder extends RecyclerView.ViewHolder {
+    @BindView(R.id.tvTitle)
+    TextView tvTitle;
+
+    @BindView(R.id.tvDescription)
+    TextView tvDescription;
+
+    @BindView(R.id.tvPriority)
+    TextView tvPriority;
 
     public ToDoViewHolder(View itemView) {
       super(itemView);
+      ButterKnife.bind(itemView);
+    }
+
+    public void bind(Task task) {
+      tvTitle.setText(task.getTitle());
+      tvDescription.setText(task.getDescription());
+      tvPriority.setText(task.getPriority());
     }
 
   }
